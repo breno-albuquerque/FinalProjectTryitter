89c2ef67425452bfcf898590caabf0ebe8e9b2fb
quesia <quesmendes@gmail.com>
2022-12-12 13:13:08 -0300

    fix: add password to student entities

--- src/Tryitter/Entities/Student.cs
+++ src/Tryitter/Entities/Student.cs
@@ -10,11 +10,10 @@ namespace Tryitter.Entities
         [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email is not valid")]
         public string Email { get; set; } = null!;
         [Required]
-        // public string Password { get; set; } = null!;
+        public string Password { get; set; } = null!;
 
-        public string FirstName { get; set; } = null!;
-
-        public string? LastName { get; set; } = null!;
+        [Required]
+        public string FullName { get; set; } = null!;
 
         public Module? Module { get; set; }
 
