6df4f9e0c10fc1bc3092d1bdf594047724839aed
Breno Albuquerque <brenoralbu@hotmail.com>
2022-12-12 21:52:26 -0300

    fix: Adapt all request classes

--- src/Tryitter/Transport/CreatePostRequest.cs
+++ src/Tryitter/Transport/CreatePostRequest.cs
@@ -1,19 +1,17 @@
-using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Tryitter.Transport
 {
     public class CreatePostRequest
     {
-        [JsonPropertyName("studentId")]
-        [Required(ErrorMessage = "O id do estudante é obrigatório")]
-            public int StudentId { get; set; }
-
         [JsonPropertyName("post")]
         [Required(ErrorMessage = "O campo post está vazio")]
         [MaxLength(300, ErrorMessage = "Seu post pode ter até 300 caracteres")]
-            public string Post { get; set; }
+        public string Post { get; set; }
 
         //preciso inserir a propriedade images aqui tb (definir se List ou Object)
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
     }
 }
--- src/Tryitter/Transport/LoginRequest.cs
+++ src/Tryitter/Transport/LoginRequest.cs
@@ -3,16 +3,13 @@ using System.Text.Json.Serialization;
 
 namespace Tryitter.Transport
 {
-    public class CreateStudentRequest
+    public class LoginRequest
     {
         [JsonPropertyName("email")]
         [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email is not valid")]
-        public string? Email { get; set; }
+        public string Email { get; set; } = null!;
 
         [JsonPropertyName("password")]
-        public string Password { get; set; }
-
-        [JsonPropertyName("fullName")]
-        public string FullName { get; set; }
+        public string Password { get; set; } = null!;
     }
 }
