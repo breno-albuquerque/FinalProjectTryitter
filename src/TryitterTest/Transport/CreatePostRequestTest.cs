529a196848cc9934b9e83b205454c3156e007f00
Breno Albuquerque <brenoralbu@hotmail.com>
2022-12-13 11:24:41 -0300

    feat: Add Transport unit tests

---
+++ src/TryitterTest/Transport/CreatePostRequestTest.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Tryitter.Transport;
+using Xunit.Sdk;
+
+namespace TryitterTest.Transport
+{
+    public class CreatePostRequestTest
+    {
+        private readonly int _studentId;
+        private readonly string _post;
+        private readonly string _image;
+
+        public CreatePostRequestTest()
+        {
+            _studentId = 1;
+            _post = "teste123";
+            _image = "teste";
+        }
+
+        [Fact(DisplayName = "Deve instanciar o objeto com propriedades corretas")]
+        public void ShouldInstanceLoginRequest()
+        {
+            //  arrange - act
+            var createPostRequest = new CreatePostRequest
+            {
+                StudentId = _studentId,
+                Post = _post,
+                Image = _image
+            };
+
+            // assert
+            AssertProperties(createPostRequest, _studentId, _post, _image);
+        }
+
+        [Fact(DisplayName = "Deve lançar excessão com propriedades inválidas")]
+        public void ShouldThrowExcepetionWithInvalidProperties()
+        {
+            //  arrange 
+            var createPostRequest = new CreatePostRequest
+            {
+                Post = _post,
+                Image = _image
+            };
+
+            //  act
+            Action act = () => AssertProperties(createPostRequest, _studentId, _post, _image);
+
+            //  assert
+            act.Should().Throw<XunitException>();
+        }
+
+        private static void AssertProperties(CreatePostRequest result, int studentId, string post, string image)
+        {
+            result.StudentId.Should().Be(studentId);
+            result.Post.Should().Be(post);
+            result.Image.Should().Be(image);
+        }
+    }
+}
---
+++ src/TryitterTest/Transport/LoginRequestTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Tryitter.Transport;
+using Xunit.Sdk;
+
+namespace TryitterTest.Transport
+{
+    public class LoginRequestTest
+    {
+        private readonly string _email;
+        private readonly string _password;
+
+        public LoginRequestTest()
+        {
+            _email = "teste@teste.com";
+            _password = "teste123";
+        }
+
+        [Fact(DisplayName = "Deve instanciar o objeto com propriedades corretas")]
+        public void ShouldInstanceObject()
+        {
+            //  arrange - act
+            var loginRequest = new LoginRequest
+            {
+                Email = _email,
+                Password = _password
+            };
+
+            // assert
+            AssertProperties(loginRequest, _email, _password);
+        }
+
+        [Fact(DisplayName = "Deve lançar excessão com propriedades inválidas")]
+        public void ShouldThrowExcepetionWithInvalidProperties()
+        {
+            //  arrange 
+            var loginRequest = new LoginRequest
+            {
+                Email = "teste",
+                Password = _password
+            };
+
+            //  act
+            Action act = () => AssertProperties(loginRequest, _email, _password);
+
+            //  assert
+            act.Should().Throw<XunitException>();
+        }
+
+        private static void AssertProperties(LoginRequest result, string email, string password)
+        {
+            result.Email.Should().Be(email);
+            result.Password.Should().Be(password);
+        }
+    }
+}
+
