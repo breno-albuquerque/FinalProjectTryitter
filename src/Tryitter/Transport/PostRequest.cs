8660c1f2ffc230b6a56c8f18048f6933b4db6ba6
Breno Albuquerque <brenoralbu@hotmail.com>
2022-12-14 12:36:30 -0300

    refactor: update PostRequest class

--- src/Tryitter/Transport/PostRequest.cs
+++ src/Tryitter/Transport/PostRequest.cs
@@ -5,10 +5,6 @@ namespace Tryitter.Transport
 {
     public class PostRequest
     {
-        [JsonPropertyName("studentId")]
-        [Required(ErrorMessage = "O id do estudante é obrigatório")]
-        public int StudentId { get; set; }
-
         [JsonPropertyName("post")]
         [Required(ErrorMessage = "O campo post está vazio")]
         [MaxLength(300, ErrorMessage = "Seu post pode ter até 300 caracteres")]
