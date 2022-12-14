522d0dba615c5a27a3bb1d07e2f12bf80872b111
quesia <quesmendes@gmail.com>
2022-12-14 13:12:09 -0300

    feat: add token generator key test

--- src/TryitterTest/TestToken.cs
+++ src/TryitterTest/TestToken.cs
@@ -16,5 +16,15 @@ namespace TryitterTest
       string response = token.Generate(student);
       response.Should().NotBeNull();
     }
+
+    [Theory(DisplayName = "TokenGenerator contains JWT 3 parts")]
+    [InlineData("teste@teste.com", "teste123")]
+    public void TestTokenGeneratorKeySuccess(string email, string password)
+    {
+      Student student = new() { Email = email, Password = password };
+      var token = new TokenGenerator().Generate(student);
+      var validTokenFormat = token.Split('.');
+      Assert.Equal(3, validTokenFormat.Length);
+    }
   }
 }
