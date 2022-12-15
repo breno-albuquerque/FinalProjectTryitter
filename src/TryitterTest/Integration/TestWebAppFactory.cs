99ba7c1e47b04a5bc7b2c27b275fcd70550fec05
Breno Albuquerque <brenoralbu@hotmail.com>
2022-12-14 21:04:38 -0300

    test: Add integration tests

---
+++ src/TryitterTest/Integration/TestWebAppFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test");
+
+        base.ConfigureWebHost(builder);
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        return base.CreateHost(builder);
+    }
+}
