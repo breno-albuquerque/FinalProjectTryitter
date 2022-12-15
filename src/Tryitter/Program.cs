770fc6fd1fef480088107b5753e329ec9ca79704
Breno Albuquerque <brenoralbu@hotmail.com>
2022-12-14 21:04:16 -0300

    test: Add Entities unit Tests

--- src/Tryitter/Program.cs
+++ src/Tryitter/Program.cs
@@ -80,3 +80,5 @@ app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
---
+++ src/TryitterTest/Entities/ModuleTest.cs
@@ -0,0 +1,23 @@
+﻿using Tryitter.Entities;
+
+namespace TryitterTest.Entities
+{
+    public class ModuleTest
+    {
+        private readonly int _moduleId = 1;
+        private readonly string _title = "test";
+
+        [Fact(DisplayName = "Deve instanciar a entidade")]
+        public void shouldInstanceEntity()
+        {
+            var module = new Module
+            {
+                ModuleId = _moduleId,
+                Title = _title,
+            };
+
+            module.ModuleId.Should().Be(_moduleId);
+            module.Title.Should().Be(_title);
+        }
+    }
+}
